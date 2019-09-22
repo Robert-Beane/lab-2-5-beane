3d4c3e830eec18bc1fbc3d06139e54d736d5175b
Robert-Beane <beane031@morris.umn.edu>
2019-09-22 16:39:05 -0500

    Add onSubmit method

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -23,4 +23,9 @@ export class CartComponent implements OnInit {
     });
   }
 
+  onSubmit(customerData){
+    console.warn('Your order has been submitted', customerData);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 }
