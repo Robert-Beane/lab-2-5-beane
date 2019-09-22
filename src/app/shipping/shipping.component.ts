d2b49270c4a928aff817049aaa372662c4b04cfd
Robert-Beane <beane031@morris.umn.edu>
2019-09-22 16:21:55 -0500

    Define shipping costs. Inject cart service

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -7,8 +7,11 @@ import {CartService} from '../cart.service';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
   }
